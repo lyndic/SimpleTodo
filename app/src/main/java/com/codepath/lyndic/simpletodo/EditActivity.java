e49fc8d1ed0a93d45c84eefa15f2fb6fe6428cec
Lyndi Castrejon <lyndic@protonmail.com>
2021-02-07 21:08:59 -0600

    Add update item ability and persistence

---
+++ app/src/main/java/com/codepath/lyndic/simpletodo/EditActivity.java
@@ -0,0 +1,46 @@
+package com.codepath.lyndic.simpletodo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EditActivity extends AppCompatActivity {
+    EditText editText;
+    Button btnSave;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+
+        editText = findViewById(R.id.etText);
+        btnSave = findViewById(R.id.btnSave);
+
+        getSupportActionBar().setTitle("Edit item");
+
+        editText.setText(getIntent().getStringExtra(MainActivity.KEY_ITEM_TEXT));
+
+        // When the user is done editing, they click this
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create an intent which will contain the results
+                Intent intent = new Intent();
+
+                // pass the results of editing
+                intent.putExtra(MainActivity.KEY_ITEM_TEXT, editText.getText().toString());
+                intent.putExtra(MainActivity.KEY_ITEM_POSITION, getIntent().getExtras().getInt(MainActivity.KEY_ITEM_POSITION));
+
+                // set the result of the intent
+                setResult(RESULT_OK, intent);
+
+                // finish the activity, close the screen and go back
+                finish();
+            }
+        });
+    }
+}
